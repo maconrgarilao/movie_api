60d43cf0ed04074ac54abb5c653e627417458a0c
maconrgarilao <108959350+maconrgarilao@users.noreply.github.com>
2023-02-15 11:48:49 -0500

    Updated CORS

--- index.js
+++ index.js
@@ -35,7 +35,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:3000', 'http://localhost:1234', 'https://myplix.netlify.app/'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            let message = 'The CORS policy for this application does not allow access from origin' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 let auth = require('./auth')(app);
 
